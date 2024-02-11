d0919c13ab33a0c2dc7c966f6fdb3971bc084c5d
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-11 13:59:40 -0500

    data displaying on page

---
+++ src/components/accordion/data.js
@@ -0,0 +1,8 @@
+const data = [
+  { id: "1", question: "blah", answer: "bloop bloop blop blip" },
+  { id: "2", question: "blah blah", answer: "bloop bloop blop" },
+  { id: "3", question: "blah blah blee", answer: "bloop bloop" },
+  { id: "4", question: "blah blah blee blah", answer: "bloop" },
+];
+
+export default data;
