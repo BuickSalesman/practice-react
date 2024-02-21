3d39546de1d075acd6a1c64c90f1438c88e22953
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-21 10:32:33 -0500

    return to video at 1:52:19

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
+
 export default function MenuItem({ item }) {
-  return <div>Menu item</div>;
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+  return (
+    <li>
+      <p>{item.label}</p>
+
+      {item && item.children && item.children.length > 0 ? <MenuList list={item.children} /> : null}
+    </li>
+  );
 }
+
+// RETURN TO VIDEO AT 1:52:19
--- src/components/tree-view/menu-list.jsx
+++ src/components/tree-view/menu-list.jsx
@@ -2,8 +2,8 @@ import MenuItem from "./menu-item";
 
 export default function MenuList({ list = [] }) {
   return (
-    <div className="menu-list-container">
+    <ul className="menu-list-container">
       {list && list.length ? list.map((listItem) => <MenuItem item={listItem} />) : null}
-    </div>
+    </ul>
   );
 }
