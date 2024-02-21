7f155c0a325b9f62cb0c2005edd88dd33d8b3749
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-21 10:22:18 -0500

    have all files in tree-view component reading from each other - starting work on tree now

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -22,6 +24,9 @@ function App() {
 
       {/* Load more products component */}
       {/* <LoadMoreData /> */}
+
+      {/* Tree view component/menu UI component */}
+      <TreeView menus={menus} />
     </div>
   );
 }
