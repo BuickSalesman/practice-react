b20e142bc4467317d87d4ca637c83d98bc18dc75
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-23 10:33:53 -0500

    QR code generating component finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Accordion from "./components/accordion";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
+import QRCodeGenerator from "./components/qr-code-generator";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import TreeView from "./components/tree-view";
@@ -26,7 +27,10 @@ function App() {
       {/* <LoadMoreData /> */}
 
       {/* Tree view component/menu UI component */}
-      <TreeView menus={menus} />
+      {/* <TreeView menus={menus} /> */}
+
+      {/* QR code generator component */}
+      <QRCodeGenerator />
     </div>
   );
 }
