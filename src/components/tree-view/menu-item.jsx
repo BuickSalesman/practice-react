934c2156bbcee1c1e7d282371aa898e1986ae0e0
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-23 09:37:17 -0500

    functionality done! time for styles

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -4,11 +4,26 @@ import MenuList from "./menu-list";
 export default function MenuItem({ item }) {
   const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
+  function handleToggleChildren(getCurrentLabel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
+
+  console.log(displayCurrentChildren);
+
   return (
     <li>
-      <p>{item.label}</p>
-
-      {item && item.children && item.children.length > 0 ? <MenuList list={item.children} /> : null}
+      <div style={{ display: "flex", gap: "20px" }}>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>{displayCurrentChildren[item.label] ? "-" : "+"}</span>
+        ) : null}
+      </div>
+      {item && item.children && item.children.length > 0 && displayCurrentChildren[item.label] ? (
+        <MenuList list={item.children} />
+      ) : null}
     </li>
   );
 }
