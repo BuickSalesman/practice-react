b736739d5231ca42c32d234479fca8815c942244
BuickSalesman <adam.odonnell.e@gmail.com>
2024-02-28 18:22:33 -0500

    almost done styling. return to video at 3:12:00

--- src/components/custom-modal-popup/modal-test.jsx
+++ src/components/custom-modal-popup/modal-test.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Modal from "./modal";
+import "./modal.css";
+import { BsTypeH1 } from "react-icons/bs";
 
 export default function ModalTest() {
   const [showModalPopup, setShowModalPopup] = useState(false);
@@ -8,10 +10,19 @@ export default function ModalTest() {
     setShowModalPopup(!showModalPopup);
   }
 
+  function onClose() {
+    setShowModalPopup(false);
+  }
+
   return (
     <div>
       <button onClick={handleToggleModalPopup}>Open Modal</button>
-      {showModalPopup && <Modal />}
+      {showModalPopup && <Modal 
+      header={}
+      onClose={onClose} body={<div>Custom body</div>} />}
     </div>
   );
 }
+
+
+// RETURN TO VIDEO AT 3:12:00
