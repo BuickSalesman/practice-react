7ce3fad41642c9194dfcba86cb89425c6ea140db
BuickSalesman <adam.odonnell.e@gmail.com>
2024-03-04 18:41:17 -0500

    got basic react structure set up

--- src/components/search-autocomplete-with-api/index.jsx
+++ src/components/search-autocomplete-with-api/index.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function SearchAutocomplete() {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  async function fetchListOfUsers() {
+    try {
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfUsers;
+  }, []);
+
   return (
     <div className="search-autocomplete-container">
-      <input name="" id="" />
+      <input name="search-users" placeholder="Search here!" />
     </div>
   );
 }
+
+// RETURN TO VIDEO AT 3:40:10
