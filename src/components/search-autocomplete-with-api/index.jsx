59d465ab9d24e4679d715d759901856100970d9a
BuickSalesman <adam.odonnell.e@gmail.com>
2024-03-10 18:51:12 -0400

    finished component! Moving on to the next one

--- src/components/search-autocomplete-with-api/index.jsx
+++ src/components/search-autocomplete-with-api/index.jsx
@@ -1,9 +1,32 @@
 import { useEffect, useState } from "react";
+import Suggestions from "./suggestions";
 
 export default function SearchAutocomplete() {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
+  const [searchParam, setSearchParam] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  function handleChange(event) {
+    const query = event.target.value.toLowerCase();
+    setSearchParam(query);
+    if (query.length > 1) {
+      const filteredData = users && users.length ? users.filter((item) => item.toLowerCase().indexOf(query) > -1) : [];
+      setFilteredUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  }
+
+  function handleClick(event) {
+    console.log(event.target.innerText);
+    setShowDropdown(false);
+    setSearchParam(event.target.innerText);
+    setFilteredUsers([]);
+  }
 
   async function fetchListOfUsers() {
     try {
@@ -28,13 +51,17 @@ export default function SearchAutocomplete() {
     fetchListOfUsers();
   }, []);
 
-  console.log(users);
+  console.log(users, filteredUsers);
 
   return (
     <div className="search-autocomplete-container">
-      <input name="search-users" placeholder="Search here!" />
+      {loading ? (
+        <h1>Loading! Please wait!</h1>
+      ) : (
+        <input value={searchParam} name="search-users" placeholder="Search here!" onChange={handleChange} />
+      )}
+
+      {showDropdown && <Suggestions handleClick={handleClick} data={filteredUsers} />}
     </div>
   );
 }
-
-// RETURN TO VIDEO AT 3:42:10
