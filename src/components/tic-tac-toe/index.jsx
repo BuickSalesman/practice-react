c91ec0e27677c8b672f3ffcd0cc27058b2f1203d
BuickSalesman <adam.odonnell.e@gmail.com>
2024-04-21 17:27:11 -0400

    working on tictactoe

--- src/components/tic-tac-toe/index.jsx
+++ src/components/tic-tac-toe/index.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import "./styles.css";
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
 export default function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  console.log(squares);
+
   return (
     <div className="tic-tac-toe-container">
-      <div className="row"></div>
-      <div className="row"></div>
-      <div className="row"></div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
   );
 }
